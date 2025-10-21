1b06d4561d24a188188704902f14a80b76981804
Sudharshan Geddam <darshans1013@gmail.com>
2025-10-21 18:46:42 +0530

    DS Concepts commit1

---
+++ DS_Concepts/LargestNumberInAnArray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class LargestNumberInAnArray{
+    void findLargest(int[] arr, int n){
+        int largestNum = arr[0];
+        if(n == 1){
+            System.out.println("Largest number in the array is: " + largestNum);
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] > largestNum){
+                    largestNum = arr[i];
+            } 
+        }
+        System.out.println("Largest number in the array is: " + largestNum);
+    }
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        LargestNumberInAnArray lnum = new LargestNumberInAnArray();
+        lnum.findLargest(arr, n);
+    }
+}
---
+++ DS_Concepts/SecondLargestNum.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SecondLargestNum {
+
+    void findSecondLargest(int[] arr, int n){
+        int largestNum = arr[0];
+        int secondLargest = 0;
+        if(n == 1){
+            System.out.println("Second Largest number not found in the array");
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] > largestNum){
+                secondLargest = largestNum;
+                largestNum = arr[i];       
+            } else if(arr[i] > secondLargest && arr[i] < largestNum){
+                secondLargest = arr[i];
+            }
+        }
+        System.out.println("Largest number in the array is: " + largestNum);
+        System.out.println("Second largest number in the array is: " + secondLargest);
+    }
+
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        SecondLargestNum lnum = new SecondLargestNum();
+        lnum.findSecondLargest(arr, n);
+    }
+}
